c607a9beb23c92f80c6852cc76de17afc1543f8c
bsmmoon <bsmmoon@gmail.com>
2018-04-11 17:16:54 +0800

    can see aero and china

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'This is my new react app',
+      currentCalendar: 'Aero',
+      calendarSrcs: {
+        'Aero': 'jb6i3e63fllvb3r6k2rprhfu78%40group.calendar.google.com',
+        'China': 'r58ht4m27g54j6k8jlqcar2p4c%40group.calendar.google.com',
+      }
     };
   }
 
-  render () {
+  makeCalendarButtons() {
+    let calendars = Object.keys(this.state.calendarSrcs);
     return (
-      <div>
-        <p>{this.state.message}</p>
+      <div className="col-xs-12">
+        {
+          calendars.map(function(calendar) {
+            return (
+              <button key={calendar} onClick={() => this.setState({currentCalendar: calendar})}>{calendar}</button>
+            )
+          }.bind(this))
+        }
+      </div>
+    )
+  }
+
+  componentDidMount() {
+  }
+
+  render () {
+    const currentCalendar = this.state.currentCalendar;
+    const src = ["https://calendar.google.com/calendar/embed?src=", this.state.calendarSrcs[currentCalendar], "&ctz=Asia%2FSingapore"].join('')
+    const calendarButtons = this.makeCalendarButtons();
+
+    return (
+      <div className="container">
+        <div className="row">
+          {calendarButtons}
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            <iframe src={src} style={{border: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+          </div>
+        </div>
       </div>
     )
   }
